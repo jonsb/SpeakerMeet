55d9345353929f6e6957b3166454ef3f83144607
Jon Bækken <jsb@mm.st>
2018-06-05 07:48:20 +0200

    Added code

---
+++ SpeakerMeet.Api/SpeakerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpeakerMeet.API
+{
+    public class SpeakerController : Controller
+    {
+        public IActionResult Search(string searchString)
+        {
+            var speakers = GetSpeakers();
+
+            var filteredSpeakers = FilterSpeakers(searchString, speakers);
+
+            if (filteredSpeakers.Any())
+            {
+                return Ok(filteredSpeakers);
+            }
+
+            return Ok(new List<Speaker>());
+        }
+
+        private static IEnumerable<Speaker> FilterSpeakers(string query, List<Speaker> speakers)
+        {
+            var filteredSpeakers = speakers.Where(s => s.Name.StartsWith(query, StringComparison.OrdinalIgnoreCase));
+            return filteredSpeakers;
+        }
+
+        private static List<Speaker> GetSpeakers()
+        {
+            var speakers = new List<Speaker>()
+            {
+                new Speaker("Joshua"),
+                new Speaker("Josh"),
+                new Speaker("Joseph")
+            };
+            return speakers;
+        }
+    }
+
+    public class Speaker
+    {
+        public Speaker(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
---
+++ SpeakerMeet.Tests/Tests.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SpeakerMeet.API;
+using Xunit;
+
+namespace SpeakerMeet.Tests
+{
+    public class Tests
+    {
+        private readonly SpeakerController _controller;
+
+        public Tests()
+        {
+            _controller = new SpeakerController();        
+        }
+
+        [Fact]
+        public void ItReturnsCollectionOfSpeakers()
+        {
+            // Arrange
+            // Act
+            var result = _controller.Search("Jobs") as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Value);
+            Assert.IsType<List<Speaker>>(result.Value);
+        }
+
+        [Fact]
+        public void GivenExactMatchThenOneSpeakerInCollection()
+        {
+            // Arrange
+            // Act
+            var result = _controller.Search("Joshua") as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            var speakers = ((IEnumerable<Speaker>) result.Value).ToList();
+            Assert.Single(speakers);
+            Assert.Equal("Joshua", speakers[0].Name);
+        }
+
+        [Theory]
+        [InlineData("Joshua")]
+        [InlineData("joshua")]
+        [InlineData("JoShUa")]
+        public void GivenCaseInsensitiveMatchThenSpeakerInCollection(string searchString)
+        {
+            // Arrange
+            // Act
+            var result = _controller.Search(searchString) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            var speakers = ((IEnumerable<Speaker>) result.Value).ToList();
+            Assert.Equal("Joshua", speakers[0].Name);
+        }
+
+        [Fact]
+        public void GivenNoMatchThenEmptyCollection()
+        {
+            // Arrange
+            // Act
+            var result = _controller.Search("ZZX") as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            var speakers = ((IEnumerable<Speaker>) result.Value).ToList();
+            Assert.Empty(speakers);
+        }
+
+        [Fact]
+        public void Given3MatchThenCollectionWith3Speakers()
+        {
+            // Arrange
+            // Act
+            var result = _controller.Search("jos") as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            var speakers = ((IEnumerable<Speaker>) result.Value).ToList();
+            Assert.Equal(3, speakers.Count);
+            Assert.Contains(speakers, s => s.Name == "Josh");
+            Assert.Contains(speakers, s=> s.Name == "Joshua");
+            Assert.Contains(speakers, s => s.Name == "Joseph");
+        }
+    }
+}
